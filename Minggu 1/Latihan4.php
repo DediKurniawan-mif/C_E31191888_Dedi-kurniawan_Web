3bc7368bd5af0cdc7e5d578edc5ae017d5a2ffea
DediKurniawan-mif <66728060+DediKurniawan-mif@users.noreply.github.com>
2021-03-14 21:47:00 +0700

    Update Latihan4.php

--- Minggu 1/Latihan4.php
+++ Minggu 1/Latihan4.php
@@ -35,6 +35,6 @@ class Buku {
 
 }
 
-$buku1 = new Buku("7 in 1 Pemrograman Web untuk Pemula", "Rohi Abdulloh", "Elex Media Komputindo", 80000, 336);
+$buku1 = new Buku("Aplikasi Millenial Menggunakan Android Studio", "Andy Kurniawan", "Gramedia", 65000, 600);
 $buku1->setDiskon(11);
 echo $buku1->getInfoBuku();
