20fcd912d42aeef437549f3f5ec4394a64f77863
DediKurniawan-mif <66728060+DediKurniawan-mif@users.noreply.github.com>
2021-03-14 22:08:17 +0700

    Update Praktikum1.php

--- Minggu 2/Praktikum1.php
+++ Minggu 2/Praktikum1.php
@@ -24,15 +24,3 @@ $mercedes->comp = 'Mercedes Benz';
 
 //call hello method untuk $bmw object.
 echo $bmw->hello();
-
-/*
-Output : Beep I am a BMW, and I am blue
-
-Penjelasan : 
-Variabel $this adalah sebuah variabel khusus dalam OOP PHP yang digunakan sebagai penunjuk 
-kepada objek, ketika kita mengaksesnya dari dalam class.
-
-Dimana Operator objek '->' digunakan dalam cakupan objek untuk mengakses method dan properti objek. 
-Artinya adalah apa yang ada di sebelah kanan operator adalah anggota dari objek 
-yang dipakai ke dalam variabel di sisi kiri operator.
-*/
