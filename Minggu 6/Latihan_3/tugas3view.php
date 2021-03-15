0b5137e7a80c6552a9eb2d26874615f03187d6bd
DediKurniawan-mif <dedikurniawan.real@gmail.com>
2021-03-15 21:36:11 +0700

    Create tugas3view.php

---
+++ Minggu 6/Latihan_3/tugas3view.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Controller, Model dan View</title>
+</head>
+<body>
+    <h2><?php echo $teks; ?></h2>
+</body>
+</html>
