39083a7d538681cd0f85d9858e4ef3cac436ba54
DediKurniawan-mif <dedikurniawan.real@gmail.com>
2021-03-15 21:36:04 +0700

    Create Tugas3.php

---
+++ Minggu 6/Latihan_3/Tugas3.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Tugas3 extends CI_Controller {
+	public function index() {
+        //c3 - memuat model 'Hello_model'
+		$this->load->model('Tugas3_model');
+
+        //pengambilan objekdari kelas Hello_model dan dimuat di var $model
+        $model = $this->Tugas3_model;
+
+        //mengambil data dari model
+        $a = $model->txt;
+
+        //membuat data yang akan dikirimkan ke view
+        $data['teks'] = $a;
+
+        //memanggil file view
+        $this->load->view('tugas3view', $data); //file view
+	}
+}
