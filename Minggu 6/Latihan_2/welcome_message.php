8626d0dbd65bcd5a6e36657b715d13c6988dcaed
DediKurniawan-mif <dedikurniawan.real@gmail.com>
2021-03-15 21:35:45 +0700

    Create welcome_message.php

---
+++ Minggu 6/Latihan_2/welcome_message.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Tugas Minggu 6</title>
+</head>
+<body>
+	<h1>Hello!</h1>
+
+		<p>Nama : Dedi Kurniawan</p>
+		<p>Nim : E31191888</p>
+		<p>Golongan : MIF C</p>
+	
+</body>
+</html>
