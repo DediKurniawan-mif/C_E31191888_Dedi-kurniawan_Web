faa27c9217d9daa88796dabaab26d7ff8c5a5bde
DediKurniawan-mif <dedikurniawan.real@gmail.com>
2021-03-15 21:39:22 +0700

    Create variabelview.php

---
+++ Minggu 6/Praktikum_3/variabelview.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Controller dan View lebih dari 1 Variabel</title>
+</head>
+<body>
+    <h2>Mengirim Data dari Controller ke View</h2>
+    
+    <!--memanggil variabel 1-->
+    variabel1: <?php echo $variabel1; ?></br>
+
+    <!--memanggil variabel 2-->
+    variabel2: <?php echo $variabel2; ?></br>
+
+</body>
+</html>
