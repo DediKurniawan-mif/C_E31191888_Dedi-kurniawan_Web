e9f3c524f8ab89b98836ee8d30a2f6af8ed98d36
DediKurniawan-mif <dedikurniawan.real@gmail.com>
2021-03-15 21:38:49 +0700

    Create helloview.php

---
+++ Minggu 6/Praktikum_1/helloview.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Hello</title>
+</head>
+<body>
+    <h2>Hello world CI!</h2>
+    <h3>Menggunakan Controllers dan View</h3>
+</body>
+</html>
